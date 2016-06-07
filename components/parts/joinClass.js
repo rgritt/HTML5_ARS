97cc8880fec38131ec69d5419392a30f86d12ab4
Ryan Gritt <rgritt@Ryans-Mac-Pro.local>
2016-06-07 14:49:23 -0500

    added join session

--- components/parts/joinClass.js
+++ components/parts/joinClass.js
@@ -4,10 +4,8 @@ var joinClass = React.createClass({
 
 	joinClass(){
 		var studentName = React.findDOMNode(this.refs.name).value;
-		var studentID = React.findDOMNode(this.refs.sid).value;
-		var seshID = React.findDOMNode(this.refs.sesh).value;
 
-		alert("The Following Data Was Collected" + studentName + "\n" + studentID + "\n" + seshID );
+		alert("The Following Data Was Collected" + studentName + "\n");
 
 
 	},
@@ -18,15 +16,9 @@ var joinClass = React.createClass({
 					<label>Full Name</label>
 					<input ref="name"
 					className="form-control"
+					placeholder="Full Name"
 					required />
-					<label>Student ID</label>
-					<input ref="sid"
-					className="form-control"
-					required />
-					<label>Session ID</label>
-					<input ref="sesh"
-					className="form-control"
-					required />
+					
 					<button className="btn btn-primary">Join Class</button>
 
 				</form>
