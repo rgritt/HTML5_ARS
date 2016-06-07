781082a025b011dae01fb5f94ede765dba7dc5d0
Ryan Gritt <ryan@igritt.com>
2016-06-07 01:09:55 -0600

    update

--- components/parts/View.js
+++ components/parts/View.js
@@ -1,8 +1,10 @@
 var React = require('react');
+
 var View = React.createClass({
 	render(){
-		retrun (this.props.if) ? <div>{this.props.children}</div> : null;
-	}
+		return (this.props.if) ? <div>{this.props.children}<div> : 
+		null;
+	};
 });
 
 module.exports = View;
